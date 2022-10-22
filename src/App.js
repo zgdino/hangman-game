4a879fa879195ca028b5f9916c88caef7f9ea6cd
Dino Hadzic <zgdino@yahoo.com>
2022-10-22 18:14:17 +0100

    Update Figure.js to show the hangman

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
     <>
       <Header />
       <div className='game-container'>
-        <Figure />
+        <Figure wrongLetters={wrongLetters}/>
         <WrongLetters wrongLetters={wrongLetters}/>
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
