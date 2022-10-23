fddac8e16c370a0c32046b15d6df3a530ff4e634
Dino Hadzic <zgdino@yahoo.com>
2022-10-23 19:17:29 +0100

    README file + comments

--- src/components/Word.js
+++ src/components/Word.js
@@ -4,14 +4,14 @@ const Word = ({ selectedWord, correctLetters }) => {
   return (
     <div className='word'>
       {selectedWord.split('').map((letter, i) => {
-        // for letters
+        // for letters - display them only if they are a part of correctLetters array
         if (/[a-z]/.test(letter)) {
           return (
             <span className='letter' key={i}>
               {correctLetters.includes(letter) ? letter : ''}
             </span>
           )
-          // for non letters
+          // for non letters - display without bottom border - refer to App.css file
         } else {
           return (
             <span className='non-letter' key={i}>
